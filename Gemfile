c0abf6599dfa6073edff24e758e6e75621c6d2b7
Perry Zheng <perry.zheng@gmail.com>
2011-07-28 20:36:12 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'sqlite3'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+group :test do
+	gem 'rspec-rails'
+	gem 'webrat'
+end
+
+# To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
+# gem 'ruby-debug'
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeadShot::Application.config.secret_token = 'cc84e23e04670426db3b314f5aa9701d88624f07228c40e7bc90475f013ce7bc5bff069329634e0ccca817237756ab2701495fa5b41a5ce06f5fc3257ea93e2c'
